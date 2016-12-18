ab2fdc10cb009e0e579c38e7fd24cc37930146a4
Amer Sawan <amersaw@gmail.com>
2016-12-18 16:27:51 +0300

    Change Audios

--- audioAssets.js
+++ audioAssets.js
@@ -3,18 +3,18 @@
 //        'url' : 'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3'
 // Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
 var audioData = [
-    {
-        'title' : 'Al-Baqarah - 31',
-		'url' : 'https://verses.quran.com/Sudais/mp3/002031.mp3'
-    },
-    {
-        'title' : 'Al-Baqarah - 32',
-		'url' : 'https://verses.quran.com/Sudais/mp3/002032.mp3'
-    },
-    {
-        'title' : 'Al-Baqarah - 33',
-		'url' : 'https://verses.quran.com/Sudais/mp3/002033.mp3'
-    },
+    {'title' : 'Al-Baqarah - 01','url' : 'https://verses.quran.com/Alafasy/mp3/002001.mp3'},
+	{'title' : 'Al-Baqarah - 02','url' : 'https://verses.quran.com/Alafasy/mp3/002002.mp3'},
+	{'title' : 'Al-Baqarah - 03','url' : 'https://verses.quran.com/Alafasy/mp3/002003.mp3'},
+	{'title' : 'Al-Baqarah - 04','url' : 'https://verses.quran.com/Alafasy/mp3/002004.mp3'},
+	{'title' : 'Al-Baqarah - 05','url' : 'https://verses.quran.com/Alafasy/mp3/002005.mp3'},
+	{'title' : 'Al-Baqarah - 06','url' : 'https://verses.quran.com/Alafasy/mp3/002006.mp3'},
+	{'title' : 'Al-Baqarah - 07','url' : 'https://verses.quran.com/Alafasy/mp3/002007.mp3'},
+	{'title' : 'Al-Baqarah - 08','url' : 'https://verses.quran.com/Alafasy/mp3/002008.mp3'},
+	{'title' : 'Al-Baqarah - 09','url' : 'https://verses.quran.com/Alafasy/mp3/002009.mp3'},
+	{'title' : 'Al-Baqarah - 10','url' : 'https://verses.quran.com/Alafasy/mp3/002010.mp3'},
+	{'title' : 'Al-Baqarah - 11','url' : 'https://verses.quran.com/Alafasy/mp3/002011.mp3'},
+	{'title' : 'Al-Baqarah - 12','url' : 'https://verses.quran.com/Alafasy/mp3/002012.mp3'},
     {
         'title' : 'Al-Lail',
         'url' : 'https://akeed.onl/092.mp3'
